e6f00e518da798cff45212bebde4eee43d311f00
Jhonbeltran <jbeltranleon@gmail.com>
2016-08-14 16:39:04 -0500

    Agrego babel-eslint para poder usar el standar junto con las nuevas caracteristicas de es

--- test.js
+++ test.js
@@ -1,9 +1,2 @@
-/*var a;*/
-/*Al ejecutar npm run lint nos va a mostrar como output:	
-standard: Use JavaScript Standard Style (http://standardjs.com) Try `standard --fix` to automatically fix problems.
-  /home/jhon/Estudio/Javascript/platzigram-db/test.js:1:2: Newline required at end of file but not found.
-  /home/jhon/Estudio/Javascript/platzigram-db/test.js:1:5: 'a' is defined but never used
-  /home/jhon/Estudio/Javascript/platzigram-db/test.js:1:6: Extra semicolon.
-*/
 var a = 'esto es correcto para standard sin los comentarios de arriba'
 console.log(a)
